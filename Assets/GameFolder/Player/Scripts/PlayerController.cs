ca23775ca45a0640af3d9c8062425e33407d041a
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-05-20 18:16:13 -0300

    [UPDATED] Dash

--- Assets/GameFolder/Player/Scripts/PlayerController.cs
+++ Assets/GameFolder/Player/Scripts/PlayerController.cs
@@ -12,8 +12,11 @@ public class PlayerController : MonoBehaviour
     public Transform skin;
     Caracters caractersController;
 
+    int dashPower = 150;
+    int jumpPower = 150;
     int comboNumber;
     float timeCombo;
+    float dashTime;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +29,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        dashTime += Time.deltaTime;
+        if (Input.GetButtonDown("Fire2") && dashTime > 2) 
+        {
+            dashTime = 0;
+            skin.GetComponent<Animator>().Play("PlayerDash", -1); // -1 search for all layers
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(skin.localScale.x * dashPower, 0));
+        }
+
         if(caractersController.life <= 0)
         {
             this.enabled = false;
         }
 
         timeCombo += Time.deltaTime;
-
         if (Input.GetButtonDown("Fire1") && timeCombo > 0.5f)
         {
             comboNumber++;
@@ -55,7 +66,7 @@ public class PlayerController : MonoBehaviour
             skin.GetComponent<Animator>().Play("PlayerJump", -1); // -1 search for all layers
             rb.velocity = Vector2.zero;
             
-            rb.AddForce(new Vector2(0, 150));
+            rb.AddForce(new Vector2(0, jumpPower));
         }
 
 
@@ -64,18 +75,24 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        speed = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        speed = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y); // Gloval var to receive speed in X with input 
 
-        rb.velocity = speed;
+        // If to check if player has used dash or not
+        if(dashTime > 0.4)
+        {
+            rb.velocity = speed; // rb velocity receive speed
+        }
+        
 
+        // If horizontal axis are =/= 0
         if(Input.GetAxisRaw("Horizontal") != 0)
         {
-            skin.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1);
-            skin.GetComponent<Animator>().SetBool("PlayerRun", true);
+            skin.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); // Flip
+            skin.GetComponent<Animator>().SetBool("PlayerRun", true); // True in animation run
         }
         else
         {
-            skin.GetComponent<Animator>().SetBool("PlayerRun", false);
+            skin.GetComponent<Animator>().SetBool("PlayerRun", false); // False in animation run
         }
     }
 }
