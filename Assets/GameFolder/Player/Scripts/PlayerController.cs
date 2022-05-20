7068772e8abf8454786c111e6ac76b6bba87d3cf
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-05-20 00:13:44 -0300

    [UPDATED] Jump caracter

--- Assets/GameFolder/Player/Scripts/PlayerController.cs
+++ Assets/GameFolder/Player/Scripts/PlayerController.cs
@@ -6,18 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     Vector2 speed;
+    
+    public Transform floorCollider;
+    public LayerMask floorLayer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool canJump = Physics2D.OverlapCircle(floorCollider.position, 0.1f, floorLayer);
+        if (canJump && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            
+            rb.AddForce(new Vector2(0, 100));
+        }
 
-        
     }
 
     private void FixedUpdate()
@@ -25,5 +35,7 @@ public class PlayerController : MonoBehaviour
         speed = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
 
         rb.velocity = speed;
+
+        
     }
 }
