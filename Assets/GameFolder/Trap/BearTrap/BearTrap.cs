a23d4d93adc04688a16c6c45f841d777e9c4183f
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-05-29 23:14:08 -0300

    [ADDED] Beartrap added in the game and code to active

---
+++ Assets/GameFolder/Trap/BearTrap/BearTrap.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+    Transform player;
+    [SerializeField] Transform skin;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")){
+            player = collision.transform;
+            collision.transform.position = transform.position;
+            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            skin.GetComponent<Animator>().Play("Stuck");
+            GetComponent<BoxCollider2D>().enabled = false;
+            collision.GetComponent<PlayerController>().enabled = false;
+            Invoke("ReleasePlayer", 1f);
+        }
+    }
+
+    void ReleasePlayer()
+    {
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+
+
+}
