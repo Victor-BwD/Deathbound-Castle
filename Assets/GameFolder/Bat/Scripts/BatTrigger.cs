16ce5fb94577a5816b7b52c989103541bc153ac0
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-13 22:52:00 -0300

    Fix bug bats

--- Assets/GameFolder/Bat/Scripts/BatTrigger.cs
+++ Assets/GameFolder/Bat/Scripts/BatTrigger.cs
@@ -5,7 +5,9 @@ namespace Bats {
         [SerializeField] Transform[] bat;
 
         private void OnTriggerEnter2D(Collider2D collision) {
-            if (collision.CompareTag("Player")) {
+            if (collision.CompareTag("Player"))
+            {
+                if (ReferenceEquals(bat, null)) return;
                 foreach (Transform obj in bat) {
                     obj.GetComponent<BatController>().enabled = true;
                 }
