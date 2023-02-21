28b631733ce7e9ba3902c49052db31cd08f3f12f
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-21 18:27:12 -0300

    Refactor addhealth script

--- Assets/GameFolder/Player/Scripts/AddHealth.cs
+++ Assets/GameFolder/Player/Scripts/AddHealth.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 namespace Player {
     public class AddHealth : MonoBehaviour {
         private void OnTriggerEnter2D(Collider2D col) {
-            if (col.CompareTag("Player")) {
-                if (col.GetComponent<Characters>().life < 5) {
-                    col.GetComponent<Characters>().life++;
-                    Destroy(this.gameObject);
-                }
+            if (col.CompareTag("Player") && col.GetComponent<Characters>().life < 5) {
+                col.GetComponent<Characters>().life++;
+                Destroy(this.gameObject);
             }
         }
     }
