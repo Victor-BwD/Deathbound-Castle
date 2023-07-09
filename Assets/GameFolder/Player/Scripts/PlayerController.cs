dcd5b43ad35e87d5470682522dadf218090f0a01
Victor Dornelles <victor.bogdanowdornelles@gmail.com>
2023-07-08 21:25:59 -0300

    Player movement refactored

--- Assets/GameFolder/Player/Scripts/PlayerController.cs
+++ Assets/GameFolder/Player/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 namespace Player
 {
     public class PlayerController : MonoBehaviour {
+
     [SerializeField] private Vector2 speed;
     [SerializeField] private Transform floorCollider;
     [SerializeField] private Transform skin;
@@ -14,99 +15,49 @@ namespace Player
 
     private Rigidbody2D rb;
     private Characters charactersController;
-    private Animator receiveSkinAnimator; // Variable to receive animator from the skin game object
-    private int dashPower = 800;
-    private int jumpPower = 1100;
-    private float dashTime;
+    private PlayerMovement playerMovement;
     private AudioPlayer audioPlayer;
     private string currentLevel;
 
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        charactersController = GetComponent<Characters>();
-        receiveSkinAnimator = skin.GetComponent<Animator>();
-        audioPlayer = GetComponent<AudioPlayer>();
+    public AudioPlayer AudioPlayer => audioPlayer;
 
-        currentLevel = SceneManager.GetActiveScene().name;
-        
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    void Update() {
-        if (!currentLevel.Equals(SceneManager.GetActiveScene().name))
-        {
-            currentLevel = SceneManager.GetActiveScene().name;
-            transform.position = GameObject.Find("Spawn").transform.position;
-        }
-        
-        dashTime += Time.deltaTime;
-        CheckDash();
-
-        DisableControls();
-
-        JumpCheck();
-    }
-
-    private void DisableControls()
-    {
-        if (charactersController.life <= 0) {
-            rb.simulated = false;
-            this.enabled = false;
-            GameOverScreen.SetActive(true);
-        }
-    }
-
-    private void JumpCheck()
-    {
-        bool canJump = Physics2D.OverlapCircle(floorCollider.position, 0.1f, floorLayer);
-        if (canJump && Input.GetButtonDown("Jump")) {
-            receiveSkinAnimator.Play("PlayerJump", -1); // -1 search for all layers
-            rb.velocity = Vector2.zero;
-
-            rb.AddForce(new Vector2(0, jumpPower));
-        }
-    }
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+            charactersController = GetComponent<Characters>();
+            audioPlayer = GetComponent<AudioPlayer>();
+            playerMovement = GetComponent<PlayerMovement>();
     
+            currentLevel = SceneManager.GetActiveScene().name;
+        
+            DontDestroyOnLoad(this.gameObject);
+        }
 
-    private void CheckDash()
-    {
-        if (Input.GetButtonDown("Fire2") && dashTime > 2)
+        void Update() 
         {
-            dashTime = 0;
-            receiveSkinAnimator.Play("PlayerDash", -1); // -1 search for all layers
-            rb.velocity = Vector2.zero;
-            rb.AddForce(new Vector2(skin.localScale.x * dashPower, 0));
-            SoundManager.Instance.Play(audioPlayer.dashSound);
-        }
-    }
+            if (!currentLevel.Equals(SceneManager.GetActiveScene().name))
+            {
+                currentLevel = SceneManager.GetActiveScene().name;
+                transform.position = GameObject.Find("Spawn").transform.position;
+            }
 
-    private void FixedUpdate()
-    {
-        speed = new Vector2(Input.GetAxisRaw("Horizontal") * 7f,
-            rb.velocity.y); // Gloval var to receive speed in X with input 
+            DisableControls();
 
-        // If to check if player has used dash or not
-        if (dashTime > 0.4) {
-            rb.velocity = speed; // rb velocity receive speed
         }
 
-        WalkAnimation();
-    }
-
-    private void WalkAnimation()
-    {
-        if (Input.GetAxisRaw("Horizontal") != 0) {
-            skin.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); // Flip
-            receiveSkinAnimator.SetBool("PlayerRun", true); // True in animation run
+        private void DisableControls()
+        {
+            if (charactersController.life <= 0) {
+                rb.simulated = false;
+                this.enabled = false;
+                playerMovement.enabled = false;
+                GameOverScreen.SetActive(true);
+            }
         }
-        else {
-            receiveSkinAnimator.SetBool("PlayerRun", false); // False in animation run
+
+
+        public void DestroyPlayer()
+        {
+            Destroy(transform.gameObject);
         }
     }
-
-    public void DestroyPlayer()
-    {
-        Destroy(transform.gameObject);
-    }
-}
 }
--- Assets/GameFolder/Player/Scripts/PlayerMovement.cs
+++ Assets/GameFolder/Player/Scripts/PlayerMovement.cs
@@ -4,8 +4,88 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Characters))]
     public class PlayerMovement : MonoBehaviour
     {
-        
+        [SerializeField] private Vector2 speed;
+        [SerializeField] private Transform floorCollider;
+        [SerializeField] private Transform skin;
+
+        public LayerMask floorLayer;
+
+        private Rigidbody2D rb;
+        private Animator receiveSkinAnimator; // Variable to receive animator from the skin game object
+        private Characters charactersController;
+        private int dashPower = 800;
+        private float dashTime;
+        private int jumpPower = 1100;
+        private PlayerController playerController;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            charactersController = GetComponent<Characters>();
+            receiveSkinAnimator = skin.GetComponent<Animator>();
+            playerController = GetComponent<PlayerController>();
+        }
+
+        private void Update()
+        {
+            dashTime += Time.deltaTime;
+            CheckDash();
+        }
+
+        private void FixedUpdate()
+        {
+            speed = new Vector2(Input.GetAxisRaw("Horizontal") * 7f, rb.velocity.y); // Global var to receive speed in X with input 
+
+            // If to check if player has used dash or not
+            if (dashTime > 0.4)
+            {
+                rb.velocity = speed; // rb velocity receive speed
+            }
+            
+            WalkAnimation();
+
+            JumpCheck();
+        }
+
+        private void WalkAnimation()
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                skin.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); // Flip
+                receiveSkinAnimator.SetBool("PlayerRun", true); // True in animation run
+            }
+            else
+            {
+                receiveSkinAnimator.SetBool("PlayerRun", false); // False in animation run
+            }
+        }
+
+        private void JumpCheck()
+        {
+            bool canJump = Physics2D.OverlapCircle(floorCollider.position, 0.1f, floorLayer);
+            if (canJump && Input.GetButtonDown("Jump"))
+            {
+                receiveSkinAnimator.Play("PlayerJump", -1); // -1 search for all layers
+                rb.velocity = Vector2.zero;
+
+                rb.AddForce(new Vector2(0, jumpPower));
+            }
+        }
+
+        private void CheckDash()
+        {
+            if (Input.GetButtonDown("Fire2") && dashTime >= 2)
+            {
+                dashTime = 0;
+                receiveSkinAnimator.Play("PlayerDash", -1); // -1 search for all layers
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(skin.localScale.x * dashPower, 0));
+                SoundManager.Instance.Play(playerController.AudioPlayer.dashSound);
+            }
+        }
     }
 }
